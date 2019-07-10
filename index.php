b641162eb82cd5cffefa033401e5b21d1dfe6da6
Tinram <Tinram@users.noreply.github.com>
2019-07-10 20:39:14 +0100

    update index.php

--- index.php
+++ index.php
@@ -26,8 +26,7 @@ use Tinram\GitHubRepoSummary\GitHubRepoSummary;
 
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=1">
         <title>GitHub Repo Summary</title>
         <meta name="application-name" content="GitHubRepoSummary">
         <meta name="description" content="Create table summary of GitHub account repo statistics.">
